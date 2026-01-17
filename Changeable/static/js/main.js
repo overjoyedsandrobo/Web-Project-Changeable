00b4e6985cbe0842d2682a111c6e248848cc6afe
Sandro Kvatashidze <sandrokvatashidze@gmail.com>
2026-01-17 11:57:53 +0400

    Initial project setup

---
+++ Changeable/static/js/main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("generation-canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+// Placeholder visual
+ctx.strokeStyle = "#555";
+ctx.strokeRect(50, 50, 500, 500);
